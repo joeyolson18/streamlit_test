b92fe0ce3ddb383930da313821bb694a506ae66a
joeyolson18 <olsonjoey83@gmail.com>
2023-10-07 21:55:56 -0600

    selector display

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ fruit_list = pd.read_csv(
     'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_list = fruit_list.set_index('Fruit')
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(
-    fruit_list.index), ['Avacado', 'Strawberries'])
+fruits_selected = streamlit.multiselect(
+    "Pick some fruits:", list(fruit_list.index))
 fruits_to_show = fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruit_list)
