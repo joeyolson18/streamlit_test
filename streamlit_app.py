8225afc852ee0abdf9f9defc780daf91c9978fc8
joeyolson18 <olsonjoey83@gmail.com>
2023-10-07 21:52:19 -0600

    selector display

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.title('MMMMMMM Food')
 
 # BREAKFAST
 streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -12,7 +13,13 @@ streamlit.text('🥑 Avacado Toast')
 
 # SMOOTHIES
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 fruit_list = pd.read_csv(
     'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
+fruit_list = fruit_list.set_index('Fruit')
+
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(
+    fruit_list.index), ['Avacado', 'Strawberries'])
+fruits_to_show = fruit_list.loc[fruits_selected]
+
 streamlit.dataframe(fruit_list)
