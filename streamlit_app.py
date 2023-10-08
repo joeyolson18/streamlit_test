d835010678a3f6196bbaaee1a646a03b513d3b33
joeyolson18 <olsonjoey83@gmail.com>
2023-10-07 21:49:03 -0600

    smoothie selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,18 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('MMMMMMM Food')
 
+# BREAKFAST
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
 
+# SMOOTHIES
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruit_list = pd.read_csv(
+    'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
+streamlit.dataframe(fruit_list)
